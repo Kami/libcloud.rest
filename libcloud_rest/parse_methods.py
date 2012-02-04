9fded1253cdcfe69c73eafe28043a2cfe6887e4f
Shawn Smith <shawnpsmith@gmail.com>
2012-02-04 01:40:54 -0800

    clean up some comments and make variable names more readable

--- libcloud_rest/parse_methods.py
+++ libcloud_rest/parse_methods.py
@@ -8,54 +8,59 @@ IGNORE_FUNCTIONS = [
     'create_node',
 ]
 
-# ignore the functions which start with _ (private functions)
-# or some functions that we don't support currently
+
+# ignore functions that start with _ (private functions)
+# and functions that we don't support currently
 def ignored_function(func_str):
     return func_str.startswith('_') or func_str in IGNORE_FUNCTIONS
 
+
 # make a dictionary of argument name, type, and whether it's optional
-# from the docstring and the fucntion information using inspect
-def get_argument(str, argspec):
+# from the docstring and the function information using inspect
+def get_argument(docstring_line, argspec):
     # expected str is following:
-    #     @type location: L{NodeLocation}
-    list = str.split(None, 2)
-    arg_name = list[1].strip(':')
-    arg_type = list[2]
+    # @type location: L{NodeLocation}
+    docstring_list = docstring_line.split(None, 2)
+    arg_name = docstring_list[1].strip(':')
+    arg_type = docstring_list[2]
     optional = False
-    # ArgSpec(args=['self', 'location'], varargs=None, keywords=None, defaults=(None,)) 
+    # ArgSpec(args=['self', 'location'], varargs=None, keywords=None, defaults=(None,))
     if argspec.defaults:
         for i, j in enumerate(argspec.defaults):
-            if argspec.args[- (i + 1)] == arg_name:
+            if argspec.args[-(i + 1)] == arg_name:
                 optional = True
 
-    return {'name' : arg_name, 'type' : arg_type, 'optional' : optional}
+    return {'name': arg_name, 'type': arg_type, 'optional': optional}
+
 
 # get the return value from the docstring
-def get_return(str):
+def get_return(docstring_line):
     regex = '(.\{.*\})'
-    val = re.search(regex, str)
+    val = re.search(regex, docstring_line)
     return val.group(0)
 
 # get the function name, argument list, return value list
 # for the functions of the NodeDriver class
 func_list = []
-for x in inspect.getmembers(NodeDriver, inspect.ismethod):
-    if not ignored_function(x[0]):
+# method_tuple looks like this:
+# ('list_images', <unbound method NodeDriver.list_images>)
+for method_tuple in inspect.getmembers(NodeDriver, inspect.ismethod):
+    if not ignored_function(method_tuple[0]):
         func_detail = {}
-        # get a function name
-        func_detail['function_name'] = x[0]
+        # get function name
+        func_detail['function_name'] = method_tuple[0]
 
-        list_docs = inspect.getdoc(x[1]).splitlines()
-        # get an argument list, return value list
+        list_docs = inspect.getdoc(method_tuple[1]).splitlines()
+        # get argument list, return value list
         list_of_arguments = []
         list_of_return_values = []
-        argspec = inspect.getargspec(x[1])
-        for x in list_docs:
-            if x.startswith('@type'):
-                list_of_arguments.append(get_argument(x, argspec))
-            elif x.startswith('@return'):
-                list_of_return_values.append(get_return(x))
-        
+        argspec = inspect.getargspec(method_tuple[1])
+        for docstring_line in list_docs:
+            if docstring_line.startswith('@type'):
+                list_of_arguments.append(get_argument(docstring_line, argspec))
+            elif docstring_line.startswith('@return'):
+                list_of_return_values.append(get_return(docstring_line))
+
         func_detail['argument_list'] = list_of_arguments
         func_detail['return_list'] = list_of_return_values
         func_list.append(func_detail)
@@ -64,9 +69,16 @@ for x in inspect.getmembers(NodeDriver, inspect.ismethod):
 # get the list of drivers that libcloud supports,
 # and list of functions that each driver has
 PROVIDERS_TO_FUNCTIONS = {}
-for k, v in DRIVERS.items():
-    t = k, v[1]
-    PROVIDERS_TO_FUNCTIONS[t] = func_list
+for driver_id, driver_tuple in DRIVERS.items():
+    driver_name = driver_tuple[1]
+    driver_id_and_name = driver_id, driver_name
+    PROVIDERS_TO_FUNCTIONS[driver_id_and_name] = func_list
 
-for k, v in PROVIDERS_TO_FUNCTIONS.items():
-    print k, v, '\n'
+if __name__ == '__main__':
+    for driver_id_and_name, func_list in PROVIDERS_TO_FUNCTIONS.items():
+        driver_name = driver_id_and_name[1]
+        print driver_name
+        for func in func_list:
+            print "Function name: %s, Argument list: %s, Return list: %s" % \
+                    (func['function_name'], func['argument_list'], func['return_list'])
+        print "=" * 80
